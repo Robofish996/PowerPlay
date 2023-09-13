4e0fcda61ce6adf25e6e5fc5ae929f8bf343d6d1
Matthew Riddle <Matthew.A.Riddle@outlook.com>
2023-09-13 13:52:13 +0200

    Fixed Title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/stylesheet.css">
-    <title>Cover Page</title>
+    <title>Power Play</title>
 </head>
 <body>
     <video id="video-background" autoplay muted loop>
